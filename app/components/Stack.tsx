2899485fc0f473ee42929a6fdc49073b13bad0fc
Anton Gunnarsson <gson.anton@hotmail.com>
2022-07-16 16:35:57 +0200

    Use gap in Stack

--- app/components/Stack.tsx
+++ app/components/Stack.tsx
@@ -1,19 +1,9 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 type Axis = "horizontal" | "vertical";
 
 export const Stack = styled.div<{ gap: number; axis?: Axis }>`
   display: flex;
   flex-direction: ${({ axis }) => (axis === "horizontal" ? "row" : "column")};
-
-  > *:not(:last-child) {
-    ${({ axis, gap }) =>
-      axis === "horizontal"
-        ? css`
-            margin-right: ${gap}px;
-          `
-        : css`
-            margin-bottom: ${gap}px;
-          `};
-  }
+  gap: ${({ gap }) => gap}px;
 `;
