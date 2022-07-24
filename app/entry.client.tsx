0678ef265c99af73a3728d8a29b720bd2f7c0d4c
Anton Gunnarsson <gson.anton@hotmail.com>
2022-07-24 16:40:28 +0000

    Add Fathom

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,24 @@
 import { RemixBrowser } from "@remix-run/react";
 import { hydrate } from "react-dom";
+import {load} from 'fathom-client'
 
-hydrate(<RemixBrowser />, document);
+function hydrateWrapper() {
+  hydrate(
+    <RemixBrowser />,
+    document
+  )
+
+  if (ENV.NODE_ENV !== 'development') {
+    load('LJDPSTZM', {
+      spa: 'history',
+      excludedDomains: ['localhost'],
+    })
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrateWrapper)
+} else {
+  window.setTimeout(hydrate, 1)
+}
