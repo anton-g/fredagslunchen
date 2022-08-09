9510e58fa619ce550cca30fa23f91fb052eedc28
Anton Gunnarsson <gson.anton@hotmail.com>
2022-08-09 11:45:14 +0200

    Add test for joining

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -5,24 +5,26 @@ describe("smoke tests", () => {
     cy.cleanupUser();
   });
 
-  // it("should allow you to register and login", () => {
-  //   const loginForm = {
-  //     email: `${faker.internet.userName()}@example.com`,
-  //     password: faker.internet.password(),
-  //   };
-  //   cy.then(() => ({ email: loginForm.email })).as("user");
+  it("should allow you to register and login", () => {
+    const loginForm = {
+      name: faker.name.firstName(),
+      email: `${faker.internet.userName()}@example.com`,
+      password: faker.internet.password(),
+    };
+    cy.then(() => ({ email: loginForm.email })).as("user");
 
-  //   cy.visit("/");
-  //   cy.findByRole("link", { name: /sign up/i }).click();
+    cy.visit("/");
+    cy.findByRole("link", { name: /join/i }).click();
 
-  //   cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-  //   cy.findByLabelText(/password/i).type(loginForm.password);
-  //   cy.findByRole("button", { name: /create account/i }).click();
+    cy.findByRole("textbox", { name: /name/i }).type(loginForm.name);
+    cy.findByLabelText(/email/i).type(loginForm.email);
+    cy.findByLabelText(/password/i).type(loginForm.password);
+    cy.findByRole("button", { name: /create account/i }).click();
 
-  //   cy.findByRole("link", { name: /notes/i }).click();
-  //   cy.findByRole("button", { name: /logout/i }).click();
-  //   cy.findByRole("link", { name: /log in/i });
-  // });
+    cy.findByRole("link", { name: /you/i }).click();
+    cy.findByRole("button", { name: /logout/i }).click();
+    cy.findByRole("link", { name: /login/i });
+  });
 
   // it("should allow you to make a note", () => {
   //   const testNote = {
