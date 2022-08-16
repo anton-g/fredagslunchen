0c86816c5410bb92c57e2cb192b5409b284a2f1e
Anton Gunnarsson <gson.anton@hotmail.com>
2022-08-16 18:02:40 +0200

    Add short intro to users without group

--- app/routes/__layout/index.tsx
+++ app/routes/__layout/index.tsx
@@ -1,23 +1,59 @@
+import { json } from "@remix-run/node";
+import type { LoaderArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 import styled from "styled-components";
+import { LinkButton } from "~/components/Button";
 import { Spacer } from "~/components/Spacer";
-import { useOptionalUser } from "~/utils";
+import { getUserId } from "~/session.server";
+import { getFullUserById } from "~/models/user.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const userId = await getUserId(request);
+
+  const user = userId
+    ? await getFullUserById({ id: userId, requestUserId: userId })
+    : null;
+
+  return json({
+    user,
+  });
+};
 
 export default function Index() {
-  const user = useOptionalUser();
+  const { user } = useLoaderData<typeof loader>();
 
-  if (user) return null;
+  if (!user) {
+    return (
+      <Wrapper>
+        <Title>Welcome.</Title>
+        <Spacer size={16} />
+        <p>
+          No more discussions about where to eat. Just get together with your
+          friends or colleagues, discover new restaurants, have a lovely time,
+          rate your lunch, soak in the statistics. And then do it all again.
+        </p>
+        <Spacer size={8} />
+        <p style={{ fontWeight: "bold" }}>Enjoy Fredagslunchen.</p>
+      </Wrapper>
+    );
+  }
+
+  if (user.groups.length > 0) return null;
 
   return (
     <Wrapper>
-      <Title>Welcome.</Title>
+      <Title>Welcome {user.name}!</Title>
       <Spacer size={16} />
       <p>
-        No more discussions about where to eat. Just get together with your
-        friends or colleagues, discover new restaurants, have a lovely time,
-        rate your lunch, soak in the statistics. And then do it all again.
+        <Link to="/groups/new">Create a new group</Link> to get started! You can
+        create a group for whatever constellation of people you want. How about
+        your team at work, that group of friends you always meet with over a
+        bowl of ramen, or maybe a group just for you?
       </p>
-      <Spacer size={8} />
-      <p style={{ fontWeight: "bold" }}>Enjoy Fredagslunchen.</p>
+      <Spacer size={24} />
+      <LinkButton to="/groups/new" variant="large">
+        Create your first group
+      </LinkButton>
     </Wrapper>
   );
 }
@@ -32,6 +68,10 @@ const Wrapper = styled.div`
   > p {
     text-align: center;
     margin: 0;
+
+    a {
+      text-decoration: underline;
+    }
   }
 `;
 
