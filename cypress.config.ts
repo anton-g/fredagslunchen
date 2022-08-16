970201ac46ffa4623b65394ff3e5f76b2335c08a
Anton Gunnarsson <gson.anton@hotmail.com>
2022-08-16 12:45:10 +0200

    Add projectId to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "wnvjrf",
   e2e: {
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
