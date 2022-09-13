18947bbe2fa4a40e148585e486000f6f2399021e
Anton Gunnarsson <gson.anton@hotmail.com>
2022-09-13 20:18:50 +0200

    Fix scroll bar jank

--- app/styles/global.ts
+++ app/styles/global.ts
@@ -1,16 +1,26 @@
 import { createGlobalStyle } from "styled-components"
 
 export default createGlobalStyle`
-  html,
+  html {
+    margin: 0;
+    min-height: 100%;
+    height: 100%;
+
+    @media screen and (min-width: 720px) {
+      margin-left: calc(100vw - 100%);
+      margin-right: 0;
+    }
+  }
+
   body {
     padding: 0;
     margin: 0;
-    font-family: Inter, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-      Helvetica Neue, sans-serif;
-    min-height: 100%;
-    height: 100%;
     background: ${({ theme }) => theme.colors.secondary};
     color: ${({ theme }) => theme.colors.primary};
+    min-height: 100%;
+    height: 100%;
+    font-family: Inter, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+      Helvetica Neue, sans-serif;
   }
 
   a {
