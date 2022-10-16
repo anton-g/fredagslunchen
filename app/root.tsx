7a684dba7d58e8ca692242ff49e0f7d5fba102ed
Anton Gunnarsson <gson.anton@hotmail.com>
2022-10-16 15:31:55 +0200

    Fix preconnect link

--- app/root.tsx
+++ app/root.tsx
@@ -64,7 +64,11 @@ function CanonicalLink({
 export const links: LinksFunction = () => {
   return [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    // { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: true },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "anonymous",
+    },
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap",
