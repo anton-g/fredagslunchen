08f9bcc7ed36d896faa4d23530937221bec31c33
Anton Gunnarsson <gson.anton@hotmail.com>
2023-06-25 22:28:49 +0200

    Set   serverModuleFormat to "cjs"

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,8 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: ["nanoid"],
+  serverDependenciesToBundle: ["nanoid", "mapbox-gl"],
+  serverModuleFormat: "cjs",
   future: {
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
